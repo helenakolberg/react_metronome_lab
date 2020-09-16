33b0ac4eaf4b1fd069e7ed909f5ff00915768c85
Helena <inger.helena@gmail.com>
2020-09-16 14:09:23 +0100

    created range input and handleInput method

--- src/components/MetronomeSetter.js
+++ src/components/MetronomeSetter.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MetronomeSetter = (props) => {
-    return (
-        <>
+class MetronomeSetter extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            tempoValue: "40"
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(event) {
+        this.setState({ tempoValue: event.target.value })
+    }
+
+    render() {
+        return (
+        <> 
+            <input
+                type="range"
+                name="tempo" 
+                id="tempo"
+                min="40"
+                max="218"
+                value={this.state.tempoValue}
+                onChange={this.handleInput}
+                />
         </>
     )
+    }
 }
 
 export default MetronomeSetter;
