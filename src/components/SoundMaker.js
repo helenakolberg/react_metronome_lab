0f507cab2e88c87a5e27820da3a3b083b1d67a5e
Helena <inger.helena@gmail.com>
2020-09-16 11:19:33 +0100

    reated MetronomeSetter stateless component, imported in MentronomeContainer

--- src/components/SoundMaker.js
+++ src/components/SoundMaker.js
@@ -1,5 +1,11 @@
-// import React from 'react';
+import React from 'react';
+
+const SoundMaker = (props) => {
+    return (
+        <>
+        </>
+    )
+}
 
 
-
-// export default SoundMaker;
+export default SoundMaker;
